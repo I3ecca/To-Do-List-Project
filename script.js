fdfa158b9a8dd192848aeca12bd274a1e838ef78
RLant <rebeccalant@gmail.com>
2020-12-23 23:45:17 -0500

    Adding script

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 
-console.log("Hello! your html and scripts are linked!")
+console.log("Hello! your html and scripts are linked!");
+
+document.getElementById("add").addEventListener("click", addFunction);
+
+function addFunction(){
+    let text = document.getElementById("input").value;
+    console.log(text);
+    let listItem = "<li>" + text + "</li>";
+    console.log(listItem);
+    console.log(typeof listItem);
+    document.getElementById("list").appendChild(listItem);
+    document.getElementById("input").value = "";
+    alert ("This addFunction function works!");
+};
