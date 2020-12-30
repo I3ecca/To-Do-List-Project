3f7890e091d2bd211713c2631e151ba4d6e4bcb4
RLant <rebeccalant@gmail.com>
2020-12-29 19:35:34 -0500

    Add function working on list

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+let addFunction = function(){
+    let text = document.getElementById("input").value;
+    let liNode = document.createElement("li");
+    let textNode = document.createTextNode(text);
+    liNode.appendChild(textNode);
+
+
+
+
+
+    document.getElementById("list").appendChild(liNode);
+    document.getElementById("input").value = "";
+    alert ("This addFunction function works!");
+};
 
 console.log("Hello! your html and scripts are linked!");
 
 document.getElementById("add").addEventListener("click", addFunction);
 
-function addFunction(){
-    let text = document.getElementById("input").value;
-    console.log(text);
-    let listItem = "<li>" + text + "</li>";
-    console.log(listItem);
-    console.log(typeof listItem);
-    document.getElementById("list").appendChild(listItem);
-    document.getElementById("input").value = "";
-    alert ("This addFunction function works!");
-};
