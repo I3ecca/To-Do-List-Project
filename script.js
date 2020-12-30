55a7421420b220ad49f8ff75b5c73d1b4b4b2b65
RLant <rebeccalant@gmail.com>
2020-12-29 23:30:44 -0500

    added delete button, functionality needed next

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ let addFunction = function(){
     let liNode = document.createElement("li");
     let textNode = document.createTextNode(text);
     liNode.appendChild(textNode);
-
-
-
-
-
     document.getElementById("list").appendChild(liNode);
     document.getElementById("input").value = "";
+    
+//create a delete button option as the li item is created//
+    let deleteBtnNode = document.createElement("button");
+    deleteBtnNode.innerHTML = "Delete";
+    liNode.appendChild(deleteBtnNode);
+
     alert ("This addFunction function works!");
 };
+
+let deleteFunction = function(){
+    let deleteBtn = 
+    document.getElementsByTagName("li")
+}
 
 console.log("Hello! your html and scripts are linked!");
 
