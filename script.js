b52c4a41797710d577ee71fe814c6e8ac3caf4ce
RLant <rebeccalant@gmail.com>
2021-01-03 18:48:33 -0500

    working delete button

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let addFunction = function(){
+let addFunction = function(event){
+    console.log(event);
     let text = document.getElementById("input").value;
     let liNode = document.createElement("li");
     let textNode = document.createTextNode(text);
@@ -9,15 +10,31 @@ let addFunction = function(){
 //create a delete button option as the li item is created//
     let deleteBtnNode = document.createElement("button");
     deleteBtnNode.innerHTML = "Delete";
+    deleteBtnNode.addEventListener("click", deleteFunction);
     liNode.appendChild(deleteBtnNode);
+//create a complete button option as il item is created//
+    let completeBtnNode = document.createElement("button");
+    completeBtnNode.innerHTML = "Complete";
+    liNode.appendChild(completeBtnNode);
 
     alert ("This addFunction function works!");
 };
 
-let deleteFunction = function(){
-    let deleteBtn = 
-    document.getElementsByTagName("li")
+let deleteFunction = function(event){
+    console.log(event.target.parentNode);
+    event.target.parentNode.remove();
+    //console.log(this.parentNode);
+   // console.log(this);
+    //let deleteBtn = document.getElementsByTagName("button").this;
+    //deleteBtn.remove();
+    
 }
+
+//let completeFunction = function(){
+    //console.log("Task Completed");
+  //  document.getElementsByTagName.button.this.parentNode.style.textDecorationLine = "line-through";
+//}
+
 
 console.log("Hello! your html and scripts are linked!");
 
